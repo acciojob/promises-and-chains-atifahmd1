ff68bec60e57e435e673a000321121815293e22a
Atif Ahmad <rising.atif@gmail.com>
2025-01-19 14:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+document.querySelector("#myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+	
+	const name = document.querySelector("#name").value;
+	const age = document.querySelector("#age").value;
+
+	const pr = new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			if(age >= 18) {
+				resolve(`Welcome ${name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		}, 4000)
+	})
+
+	pr.then((data)=>{
+		alert(data);
+	})
+	.catch((err)=>{
+		alert(err);
+	})
+});
+												   
